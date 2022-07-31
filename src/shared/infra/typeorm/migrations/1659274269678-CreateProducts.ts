03208cf175e2e27d9fb648f47b7b32b363b9eea1
Rafael Aguiar <rafael.a1@hotmail.com>
2022-07-31 10:45:19 -0300

    Create Migration to Product

---
+++ src/shared/infra/typeorm/migrations/1659274269678-CreateProducts.ts
@@ -0,0 +1,59 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateProducts1659274269678 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "products",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "description",
+                        type: "varchar"
+                    },
+                    {
+                        name: "type_id",
+                        type: "uuid"
+                    },
+                    {
+                        name: "unit_id",
+                        type: "uuid"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    }
+                ],
+                foreignKeys:[
+                    {
+                        name: "FKTypeProduct",
+                        referencedTableName: "types",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["type_id"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL"
+                    },
+                    {
+                        name: "FKUnitProduct",
+                        referencedTableName: "units",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["unit_id"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL"
+                    },                   
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("products")
+    }
+
+}
