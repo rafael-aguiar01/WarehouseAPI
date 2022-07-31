1216976e83d09602e23b3d764466d036ff17d7ab
Rafael Aguiar <rafael.a1@hotmail.com>
2022-07-31 15:49:06 -0300

    Change type_id and unit_id in Product Use Case

--- src/modules/products/useCases/CreateProduct/CreateProductUseCase.ts
+++ src/modules/products/useCases/CreateProduct/CreateProductUseCase.ts
@@ -1,21 +1,28 @@
+import { inject, injectable } from "tsyringe";
+import { AppError } from "@shared/erros/AppError";
 import { IProductsRepository } from "@modules/products/repositories/IProductsRepository";
 
 interface IRequest {
-    name: string;
-    type: string;
-    unit: string;
-    storehouse: string;
+    description: string;
+    type_id: string;
+    unit_id: string;
 }
 
-class CreateNewProductUseCase {
+@injectable()
+class CreateProductUseCase {
 
     constructor(
+        @inject("ProductRepository")
         private productsRepository: IProductsRepository
     ){}
     
-    async execute({name, type, unit, storehouse}: IRequest): Promise<void>{
-        this.productsRepository.create( { name, type, unit, storehouse  });
+    async execute({description, type_id, unit_id}: IRequest): Promise<void>{
+        const productAlreadyExists = await this.productsRepository.findByDescription(description);
+        if(productAlreadyExists){
+            throw new AppError("Product already exists!")
+        }
+        this.productsRepository.create( { description, type_id, unit_id});
     }
 }
 
-export { CreateNewProductUseCase }
+export { CreateProductUseCase }
