feac052eb49f7e57f92cb0e67b0038386c37a362
Rafael Aguiar <rafael.a1@hotmail.com>
2022-07-31 10:53:14 -0300

    Refactoring Product entities to create relationship with types and units

--- src/modules/products/infra/typeorm/entities/Product.ts
+++ src/modules/products/infra/typeorm/entities/Product.ts
@@ -1,16 +1,25 @@
+import { Column, CreateDateColumn, Entity, JoinColumn, ManyToMany, PrimaryColumn } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
+import { Type } from './Type';
+import { Unity } from './Unity';
 
+@Entity("products")
 class Product {
+    @PrimaryColumn()
     id?: string;
 
+    @Column()
     name: string;
     
-    type: string;
+    @ManyToMany(() => Type)
+    @JoinColumn({name: "type_id"})
+    type: Type;
 
+    @ManyToMany(() => Unity)
+    @JoinColumn({name: "unit_id"})
     unit: string;
 
-    storehouse: string;
-
+    @CreateDateColumn()
     created_at: Date;
 
     constructor(){
