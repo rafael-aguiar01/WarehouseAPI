8c2b62623377dc349ce8c6c395b290a6eb05a7cc
Rafael Aguiar <rafael.a1@hotmail.com>
2022-07-31 15:40:21 -0300

    Implementation ProductsRepository

---
+++ src/modules/products/infra/repositories/ProductsRepository.ts
@@ -0,0 +1,36 @@
+import { IProductsRepository} from "@modules/products/repositories/IProductsRepository";
+import { getRepository, Repository } from "typeorm";
+import { ICreateProductDTO } from "@modules/products/repositories/IProductsRepository";
+import { Product } from "../typeorm/entities/Product";
+
+class ProductRepository
+    implements IProductsRepository{
+        private repository: Repository<Product>;
+
+        constructor(){
+            this.repository = getRepository(Product)
+        }
+
+        async findByDescription(description: string): Promise<Product> {
+            const product = await this.repository.findOne({description})
+            return product;
+        }
+
+        async create({ 
+            description, 
+            type_id, 
+            unit_id
+        }: ICreateProductDTO): Promise<void> {
+            const product = this.repository.create({
+                description, 
+                type_id, 
+                unit_id 
+            });
+            console.log(product)
+
+            await this.repository.save(product)
+        }
+    }
+
+export { ProductRepository }
+
--- src/modules/products/repositories/IProductsRepository.ts
+++ src/modules/products/repositories/IProductsRepository.ts
@@ -1,16 +1,9 @@
-import { Product } from "../infra/typeorm/entities/Product"; 
-
-interface ICreateProductDTO{
-    name: string;
-    type: string;
-    unit: string;
-    storehouse: string
-}
+import { Product } from "../infra/typeorm/entities/Product";
+import { ICreateProductDTO } from '@modules/products/dtos/ICreateProductDTO'
 
 interface IProductsRepository{
-    findByName(name: string): Promise<Product>;
-    create ({name, type, unit, storehouse}: ICreateProductDTO): Promise<void>;
+    findByDescription(description: string): Promise<Product>;
+    create (data: ICreateProductDTO): Promise<void>;
 }
-
 
 export { IProductsRepository, ICreateProductDTO }
