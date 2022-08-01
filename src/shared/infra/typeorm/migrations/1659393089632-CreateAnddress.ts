c4c68c6b724222096b914afaffb73472e9b1f848
Rafael Aguiar <rafael.a1@hotmail.com>
2022-08-01 19:40:02 -0300

    Create migration to Address

---
+++ src/shared/infra/typeorm/migrations/1659393089632-CreateAnddress.ts
@@ -0,0 +1,51 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateAnddress1659393089632 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name:"address",
+                columns:[
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "storehouse_id",
+                        type: "varchar",
+                    },
+                    {
+                        name: "code",
+                        type: "varchar",
+                    },
+                    {
+                        name: "capacity",
+                        type: "varchar",
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()",
+                    }
+                ],
+                foreignKeys:[
+                    {
+                        name: "FKStorehouse",
+                        referencedTableName: "storehouses",
+                        referencedColumnNames: ["id"],
+                        columnNames: ["storehouse_id"],
+                        onDelete: "SET NULL",
+                        onUpdate: "SET NULL"
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("storehouses")
+    }
+
+}
