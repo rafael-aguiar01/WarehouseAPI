8bcd12eaea75dab4ef6252440f5f44ca145709f7
Rafael Aguiar <rafael.a1@hotmail.com>
2022-08-06 10:54:53 -0300

    Create Use Case to delete unit

--- src/modules/products/useCases/DeleteUnity/DeleteUnityUseCase.ts
+++ src/modules/products/useCases/DeleteUnity/DeleteUnityUseCase.ts
@@ -1,20 +1,27 @@
 import { IUnitsRepository } from "@modules/products/repositories/IUnitsRepository"
+import { AppError } from "@shared/erros/AppError";
+import { inject, injectable } from "tsyringe"
 
 
 interface IRequest{
     id: string
 }
 
+@injectable()
 class DeleteUnityUseCase {
 
     constructor(
+        @inject("UnitsRepository")
         private unitsRepository: IUnitsRepository
     ){}
 
     async execute ({ id }: IRequest): Promise<void>{
-        this.unitsRepository.delete({ id })
+        const unitAlreadyExists = await this.unitsRepository.findByID(id);
+        if(!unitAlreadyExists){
+            throw new AppError("Unit no exists!");
+        }
+        this.unitsRepository.deleteByDescription({ id })
     }
 }
-
 
 export { DeleteUnityUseCase}
