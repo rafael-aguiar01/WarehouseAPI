9a6e7d39b733a30a5662d83297af44cc6b4107a9
Rafael Aguiar <rafael.a1@hotmail.com>
2022-08-06 13:16:56 -0300

    Edit Address routes to delete address

--- src/shared/infra/http/routes/address.routes.ts
+++ src/shared/infra/http/routes/address.routes.ts
@@ -1,14 +1,22 @@
 
+import { DeleteAddressController } from '@modules/warehouse/useCases/DeleteAddress/DeleteAddressController';
 import { CreateAddressController } from '@modules/warehouse/useCases/CreateAddress/CreateAddressUseCaseController';
+
 import { Router } from 'express';
 
 const addressRoutes = Router();
 
-const createAddressController = new CreateAddressController
+const createAddressController = new CreateAddressController();
+const deleteAddressController = new DeleteAddressController();
 
 addressRoutes.post(
     "/",
     createAddressController.handle
+);
+
+addressRoutes.delete(
+    "/:id",
+    deleteAddressController.handle
 );
 
 export { addressRoutes }
