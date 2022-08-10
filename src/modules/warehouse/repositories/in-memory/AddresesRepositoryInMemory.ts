7fe3c83aaefdfb0327909bb7405dded0402b78f7
Rafael Aguiar <rafael.a1@hotmail.com>
2022-08-10 15:01:30 -0300

    Fix testes to create address

--- src/modules/warehouse/repositories/in-memory/AddresesRepositoryInMemory.ts
+++ src/modules/warehouse/repositories/in-memory/AddresesRepositoryInMemory.ts
@@ -1,24 +1,33 @@
 import { Address } from "@modules/warehouse/infra/typeorm/entities/Address";
-import { IAddressRepository,  ICreateAddressDTO } from "../IAddresesRepository";
+import { IAddressRepository,  ICreateAddressDTO, IDeleteAddressDTO } from "../IAddresesRepository";
 
 class AddressRepositoryInMemory implements IAddressRepository {
 
     addreses: Address[] = [];
 
-    async create({ name }: ICreateAddressDTO): Promise<void> {
+    async create({ storehouse_id, code, capacity  }: ICreateAddressDTO): Promise<void> {
         const address = new Address();
 
         Object.assign(address, {
-            name,
+            storehouse_id,
+            code,
+            capacity
         });
 
         this.addreses.push(address);
     }
-    
-    async findByName(name: string): Promise<Address> {
-        const address = this.addreses.find((address) => address.name === name);
+    async findByCode(code: string): Promise<Address> {
+        const address = this.addreses.find((address) => address.code === code);
         return address;
     }
+    async findByID(id: string): Promise<Address> {
+        const address = this.addreses.find((address) => address.id === id);
+        return address;
+    }
+    async deleteById({ id }: IDeleteAddressDTO): Promise<void> {
+        this.addreses = this.addreses.filter((adress) => adress.id !== id)
+    }
+
 
 }
 
--- src/modules/warehouse/useCases/CreateAddress/CreateAddressUseCase.spec.ts
+++ src/modules/warehouse/useCases/CreateAddress/CreateAddressUseCase.spec.ts
@@ -1,33 +1,60 @@
 
 import { AddressRepositoryInMemory } from "@modules/warehouse/repositories/in-memory/AddresesRepositoryInMemory";
-import { CreateAddressInWarehouseUseCase } from "./CreateAddressUseCase";
+import { AppError } from "@shared/erros/AppError";
+import { CreateAddressUseCase } from "./CreateAddressUseCase";
 
-let createAddressInWarehouseUseCase: CreateAddressInWarehouseUseCase;
+let createAddressUseCase: CreateAddressUseCase;
 let addressRepositoryInMemory: AddressRepositoryInMemory;
 
 describe( 'Create Address', () =>{
 
     beforeEach(() => {
         addressRepositoryInMemory = new AddressRepositoryInMemory();
-        createAddressInWarehouseUseCase = new CreateAddressInWarehouseUseCase(
+        createAddressUseCase = new CreateAddressUseCase(
             addressRepositoryInMemory
         );
     })
 
     it('should be able to create a new address', async() => {
         const address = {
-            name: "12B-X7"
+            storehouse_id: "12345",
+            code: "12B-X7",
+            capacity: 55
         };
 
-        await createAddressInWarehouseUseCase.execute({
-            name: address.name,
+        await createAddressUseCase.execute({
+            storehouse_id: address.storehouse_id,
+            code: address.code,
+            capacity: address.capacity
         })
 
-        const addressCreated = await addressRepositoryInMemory.findByName(
-            address.name
+        const addressCreated = await addressRepositoryInMemory.findByCode(
+            address.code
         )
 
         expect(addressCreated).toHaveProperty('id')
+    })
+
+    it('should not be able to create a address with exists code', async() =>{
+        const address = {
+            storehouse_id: "12345",
+            code: "12B-X7",
+            capacity: 55
+        };
+
+        await createAddressUseCase.execute({
+            storehouse_id: address.storehouse_id,
+            code: address.code,
+            capacity: address.capacity
+        })
+
+        await expect(createAddressUseCase.execute({
+            storehouse_id: address.storehouse_id,
+            code: address.code,
+            capacity: address.capacity
+        })
+        ).rejects.toEqual(new AppError("Address already exists!"))
+
     })
 
 })
