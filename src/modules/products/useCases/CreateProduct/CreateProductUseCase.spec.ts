031fdff1f1b88c0b6758b83d217658e6e54441fe
Rafael Aguiar <rafael.a1@hotmail.com>
2022-08-11 20:30:32 -0300

    Create tests to check create Product

--- src/modules/products/useCases/CreateProduct/CreateProductUseCase.spec.ts
+++ src/modules/products/useCases/CreateProduct/CreateProductUseCase.spec.ts
@@ -1,40 +1,57 @@
 import { ProductsRepositoryInMemory } from "@modules/products/repositories/in-memory/ProductsRepositoryInMemory";
-import { CreateNewProductUseCase } from "./CreateProductUseCase";
+import { AppError } from "@shared/erros/AppError";
+import { CreateProductUseCase } from "./CreateProductUseCase";
 
-let createNewProductUseCase: CreateNewProductUseCase;
+let createNewProductUseCase: CreateProductUseCase;
 let productsRepositoryInMemory: ProductsRepositoryInMemory;
 
 describe ('Create Product', () => {
 
     beforeEach(() => {
         productsRepositoryInMemory = new ProductsRepositoryInMemory();
-        createNewProductUseCase = new CreateNewProductUseCase(
+        createNewProductUseCase = new CreateProductUseCase(
             productsRepositoryInMemory
         );
     })
 
     it('should be able to create a new product', async() =>{
         const product = {
-            name: 'Product Test',
-            type: '1',
-            unit: '1',
-            storehouse: '1',
+            description: 'Product Test',
+            type_id:'1234',
+            unit_id:'1222'
         };
 
         await createNewProductUseCase.execute({
-            name: product.name,
-            type: product.type,
-            unit: product.unit,
-            storehouse: product.storehouse
+            description: product.description,
+            type_id: product.type_id,
+            unit_id: product.unit_id
         })
         
-        const productCreated = await productsRepositoryInMemory.findByName(
-            product.name,
+        const productCreated = await productsRepositoryInMemory.findByDescription(
+            product.description,
         )
 
         expect(productCreated).toHaveProperty('id')
-        expect(productCreated).toHaveProperty('type')
-        expect(productCreated).toHaveProperty('unit')
-        expect(productCreated).toHaveProperty('storehouse')
+        expect(productCreated).toHaveProperty('type_id')
+        expect(productCreated).toHaveProperty('unit_id')
+    })
+
+    it('should not be able to create a product with exists name', async()=> {
+        const product = {
+            description: 'Product Test',
+            type_id:'1234',
+            unit_id:'1222'
+        };
+        await createNewProductUseCase.execute({
+            description: product.description,
+            type_id: product.type_id,
+            unit_id: product.unit_id
+        })
+        await expect(createNewProductUseCase.execute({
+            description: product.description,
+            type_id: product.type_id,
+            unit_id: product.unit_id
+        })).rejects.toEqual(new AppError("Product already exists!"))
+
     })
 });
